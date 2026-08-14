815f95248af98551b4ea3dd9d087d27cc2311dd0
Nuclrdev1 <dev@nuclr.dev>
2026-08-14 18:49:14 +1000

    Updated to SDK 4.0.0

--- src/main/java/dev/nuclr/plugin/core/quick/viewer/jvm/ClassQuickViewPlugin.java
+++ src/main/java/dev/nuclr/plugin/core/quick/viewer/jvm/ClassQuickViewPlugin.java
@@ -75,10 +75,6 @@ public class ClassQuickViewPlugin implements QuickViewNuclrPlugin {
 		return name.substring(dot + 1);
 	}
 	
-	@Override
-	public int priority() {
-		return 1;
-	}
 
 	@Override
 	public boolean openResource(NuclrResource resource, AtomicBoolean cancelled) {
@@ -122,75 +118,8 @@ public class ClassQuickViewPlugin implements QuickViewNuclrPlugin {
 		return false;
 	}
 
-	private String name = "JVM Class Quick Viewer";
 	private String id = "dev.nuclr.plugin.core.quickviewer.jvm";
-	private final String version = loadVersion();
-	private String description = "Decompiles and previews Java .class files using Vineflower.";
-	private String author = "Nuclr Development Team";
-	private String license = "Apache-2.0";
-	private String website = "https://nuclr.dev";
-	private String pageUrl = "https://nuclr.dev/plugins/core/jvm-quick-viewer.html";
-	private String docUrl = "https://nuclr.dev/plugins/core/jvm-quick-viewer.html";
 
-	@Override
-	public String id() {
-		return id;
-	}
-
-	@Override
-	public String name() {
-		return name;
-	}
-
-	@Override
-	public String version() {
-		return version;
-	}
-	private static String loadVersion() {
-		try (var stream = ClassQuickViewPlugin.class.getResourceAsStream("/plugin.properties")) {
-			if (stream == null) return "unknown";
-			var props = new java.util.Properties();
-			props.load(stream);
-			return props.getProperty("version", "unknown");
-		} catch (java.io.IOException e) {
-			return "unknown";
-		}
-	}
-
-	@Override
-	public String description() {
-		return description;
-	}
-
-	@Override
-	public String author() {
-		return author;
-	}
-
-	@Override
-	public String license() {
-		return license;
-	}
-
-	@Override
-	public String website() {
-		return website;
-	}
-
-	@Override
-	public String pageUrl() {
-		return pageUrl;
-	}
-
-	@Override
-	public String docUrl() {
-		return docUrl;
-	}
-
-	@Override
-	public Developer developer() {
-		return Developer.Official;
-	}
 
 	@Override
 	public void updateTheme(NuclrThemeScheme themeScheme) {
@@ -204,7 +133,8 @@ public class ClassQuickViewPlugin implements QuickViewNuclrPlugin {
 
 	@Override
 	public String uuid() {
-		return id();
+		return id;
 	}
+
 
 }
